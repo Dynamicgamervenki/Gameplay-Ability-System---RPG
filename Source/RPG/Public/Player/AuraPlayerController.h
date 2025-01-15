042d59b172796975383edaff6131097a74c87e7e
Venki <18534venkanna@gmail.com>
2025-01-15 12:31:41 +0530

    Player Controller

--- Source/RPG/Public/Player/AuraPlayerController.h
+++ Source/RPG/Public/Player/AuraPlayerController.h
@@ -13,5 +13,12 @@ UCLASS()
 class RPG_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+public :
+	AAuraPlayerController();
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere,Category=Input)
+	TObjectPtr<class UInputMappingContext> AuraContext;	
 };
