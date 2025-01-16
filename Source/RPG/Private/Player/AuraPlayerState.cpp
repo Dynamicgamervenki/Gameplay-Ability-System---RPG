675aa74fbb62c401ce4504ad765bcf41fe4c1271
Venki <18534venkanna@gmail.com>
2025-01-16 09:07:14 +0530

    The Player State

---
+++ Source/RPG/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright DynamicVenki
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.0f;000
+}
---
+++ Source/RPG/Public/Player/AuraPlayerState.h
@@ -0,0 +1,19 @@
+// Copyright DynamicVenki
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPG_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AAuraPlayerState();
+};
